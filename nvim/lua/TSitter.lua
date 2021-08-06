8f857eff13b5c8bf36836d2813016df954da2839
Vu Nguyen <nxvu699134@gmail.com>
2021-08-07 03:03:35 +0700

    Forget remove unused files

--- nvim/lua/TSitter.lua
+++
@@ -1,9 +0,0 @@
-local ts = require 'nvim-treesitter.configs'
-ts.setup {
-  ensure_installed = {'html', 'css', 'json', 'jsdoc', 'javascript', 'typescript', 'python', 'lua', 'tsx', 'svelte'},
-  -- indent = { enable = true },
-  highlight = {
-    enable = true,
-    use_languagetree = true
-  },
-}
--- nvim/lua/git.lua
+++
@@ -1,8 +0,0 @@
-local u = require('utils')
-
------------------------VIM FUGITIVE-----------------------------
-vim.cmd('set diffopt+=vertical')
-u.map('', '<Leader>gb', ':Git blame<CR>')
-
------------------------VIM GITGUTTER-----------------------------
-vim.g.gitgutter_sign_priority = 4
